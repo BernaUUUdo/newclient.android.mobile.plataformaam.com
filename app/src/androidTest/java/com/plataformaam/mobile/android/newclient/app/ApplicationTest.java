367aaad9bf44cffca55808e47fbc62f8b9f72d0d
Bernard Corrêa Pereira <bernauuudo@yahoo.com.br>
2015-11-05 17:11:54 -0200

    Login e Design de Interface -
      Protótipo da interface do login

--- app/src/androidTest/java/com/plataformaam/mobile/android/newclient/app/ApplicationTest.java
+++ app/src/androidTest/java/com/plataformaam/mobile/android/newclient/app/ApplicationTest.java
@@ -2,10 +2,8 @@ package com.plataformaam.mobile.android.newclient.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
