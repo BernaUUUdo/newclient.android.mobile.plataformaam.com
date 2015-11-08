e48b1642975072a2963e805c3010ba3bea91b176
Bernard Corrêa Pereira <bernauuudo@yahoo.com.br>
2015-11-08 20:12:53 -0200

    Sessão de usuário - Gerenciamento Comentários no código para esclarecer o resto da equipe !
    Convertendo testes para JUNIT 4

--- app/src/androidTest/java/com/plataformaam/mobile/android/newclient/app/session/SessionManagerTest.java
+++ app/src/androidTest/java/com/plataformaam/mobile/android/newclient/app/session/SessionManagerTest.java
@@ -1,48 +1,61 @@
 package com.plataformaam.mobile.android.newclient.app.session;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.plataformaam.mobile.android.newclient.app.models.User;
 
 import junit.framework.TestCase;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class SessionManagerTest extends TestCase {
+@RunWith(AndroidJUnit4.class)
+public class SessionManagerTest {
 
+    @Before
     public void setUp(){
 
     }
 
+    @After
     public void tearDown(){
 
     }
 
     //Testa se o login foi feito
+    @Test
     public void test_doLogin(){
         SessionManager sessionManager = SessionManager.getInstance();
         User user = new User();
         sessionManager.doLogin(user);
-        assertTrue( sessionManager.isLogged() );
+        Assert.assertTrue(sessionManager.isLogged());
     }
 
     //Testa se o Logout foi feito
+    @Test
     public void test_doLogout(){
         SessionManager sessionManager = SessionManager.getInstance();
         User user = new User();
         sessionManager.doLogin(user);
-        assertTrue(sessionManager.isLogged());
+        Assert.assertTrue(sessionManager.isLogged());
         sessionManager.doLogut();
-        assertFalse(sessionManager.isLogged());
+        Assert.assertFalse(sessionManager.isLogged());
 
     }
 
     //Teste a obtenção de um usuário com o sistema logado
+    @Test
     public void test_getLoggedUser(){
         SessionManager sessionManager = SessionManager.getInstance();
         User user = new User();
         user.setId(1);
         sessionManager.doLogin(user);
-        assertTrue( sessionManager.isLogged() );
+        Assert.assertTrue(sessionManager.isLogged());
         User verifyUser = SessionManager.getInstance().getLoggedUser();
-        assertEquals(user.getId(), verifyUser.getId() );
+        Assert.assertEquals(user.getId(), verifyUser.getId());
     }
 
 
